d3c2d484cf81a770215d7fdcfb491f2a3a101a60
PRIYANKA DAS <PRIYANKA DAS@DESKTOP-1RFIOMO>
2022-03-26 10:12:32 +0530

    commit 4

---
+++ DemoProject/src/main/java/com/demo/Service/StudentService.java
@@ -0,0 +1,38 @@
+package com.demo.Service;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.Model.Student;
+import com.demo.Repository.StudentRepo;
+
+
+@Service
+@Transactional
+public class StudentService {
+
+	@Autowired
+	StudentRepo repo;
+
+	public Student addStudent(Student s) {
+		return repo.save(s);
+	}
+
+	public Optional<Student> findByStudentId(int studentId) {
+		return repo.findByStudentId(studentId);
+	}
+	
+	
+	
+
+	public void updateByStudentId(Student s) {
+		repo.save(s);
+	}
+
+	
+
+}
