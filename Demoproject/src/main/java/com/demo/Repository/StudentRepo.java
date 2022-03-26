e04a38beedd75de37c1b5fdf1f7b46f3959e7a0f
DELL <DELL@DESKTOP-OV95KM1>
2022-03-26 10:35:51 +0530

    Student Repo

---
+++ Demoproject/src/main/java/com/demo/Repository/StudentRepo.java
@@ -0,0 +1,17 @@
+package com.demo.Repository;
+
+
+	import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.demo.Model.Student;
+	@Repository
+	public interface StudentRepo extends JpaRepository<Student, Integer> {
+	    Optional<Student> findByStudentId(int studentId);
+	}
+	
+	
+
+
